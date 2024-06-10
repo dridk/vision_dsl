102b72fa5ffa5bc827dabe5aa8cd7cafc78c0f60
Bertrand Borel <95342688+BertrandBorel@users.noreply.github.com>
2024-06-10 16:03:52 +0200

    Add files via upload

--- dsl_test.py
+++ dsl_test.py
@@ -1,19 +1,26 @@
 from textx import metamodel_from_file
 # textx generate query.tx --grammar dsl.tx --target dot
 
-def cast_value(x):
-    if type(x) == str :
-        return f"'{x}'"
+def cast_value(x, operator="pass"):
+    if type(x) == int :
+        return f"CAST(value AS INTEGER){operator}{x}"
+    if type(x) == float :
+        return f"CAST(value AS FLOAT){operator}{x}"
     else :
-        return x
+        return f"value='{x}'"
 
 def simple_query(element:str) -> str:
-    # prend un requête simple : "bio:fer > 10 AND pmsi:cim10 = 'FT43ATC'"
-    # prend le premier + l'opérateur (AND/OR) + le dernier élément 
-    request = f"""
-        SELECT IPP FROM data WHERE domain='{element[0].op.field.domain}' AND key='{element[0].op.field.key}' AND value{element[0].op.op}{cast_value(element[0].op.val)}
-        {element[1]} IPP IN (SELECT IPP FROM data WHERE domain='{element[-1].op.field.domain}' AND key='{element[-1].op.field.key}' AND value{element[-1].op.op}{cast_value(element[-1].op.val)})
-        """
+    # exemple : DOMAINE='x' value > 10
+    if len(element) == 1 :
+        request = f"""SELECT IPP FROM data WHERE domain='{element[0].op.field.domain}' AND key='{element[0].op.field.key}' AND {cast_value(element[0].op.val, element[0].op.op)}"""
+    # exemple"bio:fer > 10 AND pmsi:cim10 = 'FT43ATC'"
+    # prend le premier + l'opérateur (AND/OR):element[1] + le dernier élément 
+    else : 
+        if element[1] == "AND" :
+            operator = "\nINTERSECT\n"
+        if element[1] == "OR" :
+            operator = "\nUNION\n"
+        request = f"""SELECT IPP FROM data WHERE domain='{element[0].op.field.domain}' AND key='{element[0].op.field.key}' AND {cast_value(element[0].op.val, element[0].op.op)}{operator}(SELECT IPP FROM data WHERE domain='{element[-1].op.field.domain}' AND key='{element[-1].op.field.key}' AND {cast_value(element[-1].op.val, element[-1].op.op)})"""
     return request
 
 
@@ -21,7 +28,7 @@ def simple_query(element:str) -> str:
 def query_to_sql(query:str) -> str:
     metamodel = metamodel_from_file("dsl.tx")
     model = metamodel.model_from_str(query)
-    # si la requête est simple (2 éléments)
+    # si la requête est simple (2 éléments + 1 opérateur)
     if len(model.op) <= 3 :
         return simple_query(model.op)
    
@@ -36,25 +43,20 @@ def query_to_sql(query:str) -> str:
         position_dict[i] = value
         
     # 1er élément de la requête  
-    request = "SELECT IPP FROM data WHERE " + f"domain='{model.op[0].op.field.domain}' AND key='{model.op[0].op.field.key}' AND value{model.op[0].op.op}{cast_value(model.op[0].op.val)}"
+    request = "SELECT IPP FROM data WHERE " + f"domain='{model.op[0].op.field.domain}' AND key='{model.op[0].op.field.key}' AND {cast_value(model.op[0].op.val, model.op[0].op.op)}"
     # suppression du premier élément du dictionnaire
     del position_dict[0]
-
-    # INTERSECT
-    request = request + " INTERSECT ("
-    # chaque élément est inséré précédé d'UNION
-    for key, value in position_dict.items() :
-        # Si None = pas un mots-clés
-        #### REGLER ICI AND ET OR ##################################
-        if value is None:
-            if type(model.op[key].op.val) == int:
-                request = request + f"SELECT IPP FROM data WHERE domain='{model.op[key].op.field.domain}' AND key='{model.op[key].op.field.key}' AND value{model.op[key].op.op}{model.op[key].op.val} UNION "
-            else : 
-                request = request + f"SELECT IPP FROM data WHERE domain='{model.op[key].op.field.domain}' AND key='{model.op[key].op.field.key}' AND value{model.op[key].op.op}'{model.op[key].op.val}' UNION "
-
-    request = request + ")"
-    request = request.replace(" UNION )", ")")
-
+    
+    for key, value in position_dict.items() :        
+        if value is None: 
+            request = request + f"(SELECT IPP FROM data WHERE domain='{model.op[key].op.field.domain}' AND key='{model.op[key].op.field.key}' AND {cast_value(model.op[key].op.val, model.op[0].op.op)})"
+        else :
+            if value == "AND" :
+                operator = "\nINTERSECT\n"
+            if value == "OR" :
+                operator = "\nUNION\n"
+            request = request + f" {operator}"
+    
     return request
     
 
@@ -62,11 +64,17 @@ def query_to_sql(query:str) -> str:
 
 # Requêtes de test :
 # ------------------------------------
-#query="bio:Fer = 10 AND pmsi:CIM10 = 'D329' OR pmsi:CIM10 = 'C000'"
-#query="bio:Fer = 10 AND pmsi:CIM10 = 'E435'"
+# query = "bio:Fer < 10"
+#query = "bio:Fer < 10.5"
+# query = "pmsi:CIM10 = 'E435'"
+#query = "bio:Fer > 10 OR pmsi:CIM10 = 'E30'"
+# query="bio:Fer > 5 AND pmsi:CIM10 = 'E435'"
 #query="bio:Fer = 10 OR pmsi:CIM10 = 'N99'"
-# query="bio:Fer = 10 AND pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002'"
-# query="bio:Fer = 10 AND pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002' OR bio:Fer = 50"
+#query="bio:Fer = 10 AND pmsi:CIM10 = 'D329' OR pmsi:CIM10 = 'C000'"
+#query="bio:Fer = 10 AND pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002'"
+#query="bio:Fer = 10 AND pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002'"
+#query="bio:Fer = 10 AND pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002' OR bio:Fer = 50"
+#query="bio:Fer = 10 OR pmsi:CIM10 = 'S031' OR pmsi:CIM10 = 'C000' AND pharma:CCAM = 'HBQK002' OR bio:Fer = 50"
 
 
 # result_query = query_to_sql(query)   
